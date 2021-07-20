0e4139453346fe1d4f21a1a2784fe64ce9104a1c
Matheus Ferreira <matheus.44300@gmail.com>
2021-07-20 16:46:01 -0300

    Added Field Type to Text Field

--- Bibliotech/UserControls/TextField.xaml.cs
+++ Bibliotech/UserControls/TextField.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Bibliotech.UserControls.CustomEnums;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Bibliotech.UserControls
 {
@@ -24,6 +15,7 @@ namespace Bibliotech.UserControls
         public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(TextField), new PropertyMetadata("Title"));
         public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(TextField), new PropertyMetadata(string.Empty));
         public static readonly DependencyProperty MaxLengthProperty = DependencyProperty.Register("MaxLength", typeof(int), typeof(TextField), new PropertyMetadata(0));
+        public static readonly DependencyProperty FieldTypeProperty = DependencyProperty.Register("FieldType", typeof(FieldType), typeof(TextField), new PropertyMetadata(FieldType.String));
 
         public string Placeholder
         {
@@ -49,6 +41,12 @@ namespace Bibliotech.UserControls
             set => SetValue(MaxLengthProperty, value);
         }
 
+        public FieldType FieldType
+        {
+            get => (FieldType)GetValue(FieldTypeProperty);
+            set => SetValue(FieldTypeProperty, value);
+        }
+
         public TextField()
         {
             InitializeComponent();
@@ -57,6 +55,17 @@ namespace Bibliotech.UserControls
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             placeholder.Visibility = string.IsNullOrEmpty(textBox.Text) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            switch (FieldType)
+            {
+                case FieldType.Number:
+                    Regex regex = new Regex("[^0-9]+");
+                    e.Handled = regex.IsMatch(e.Text);
+                    break;
+            }
         }
     }
 }
