fd22c7e39a5240c4046bfbdd428db002de621d18
Matheus Ferreira <matheus.44300@gmail.com>
2021-07-20 16:05:45 -0300

    Finished design of Server Window

---
+++ Bibliotech/Singletons/Session.cs
@@ -0,0 +1,24 @@
+﻿namespace Bibliotech.Singletons
+{
+    public class Session
+    {
+        public static Session Instance
+        {
+            get
+            {
+                if (session == null)
+                    session = new Session();
+
+                return session;
+            }
+        }
+        private static Session session;
+
+        public Server Server { get; set; }
+
+        private Session()
+        {
+            Server = new Server();
+        }
+    }
+}
