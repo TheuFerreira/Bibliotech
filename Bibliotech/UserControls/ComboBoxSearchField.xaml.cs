3f93b0617a635dee08db73bad53842d644a5fd59
Matheus Ferreira <matheus.44300@gmail.com>
2021-07-22 10:45:56 -0300

    Update ComboBox Search Field

--- Bibliotech/UserControls/ComboBoxSearchField.xaml.cs
+++ Bibliotech/UserControls/ComboBoxSearchField.xaml.cs
@@ -20,9 +20,27 @@ namespace Bibliotech.UserControls
     /// </summary>
     public partial class ComboBoxSearchField : UserControl
     {
+        public string Text
+        {
+            get => tb.Text;
+            set => tb.Text = value;
+        }
+
+        public event RoutedEventHandler Click;
+
         public ComboBoxSearchField()
         {
             InitializeComponent();
+        }
+
+        private void Tb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            tbPlaceholder.Visibility = string.IsNullOrEmpty(tb.Text) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Click?.Invoke(sender, e);
         }
     }
 }
