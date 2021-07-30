0041e1b1e0e32c6a5c51a1a5ff2e79fcba660891
Matheus Ferreira <matheus.44300@gmail.com>
2021-07-30 01:22:07 -0300

    Added Custom ComboBox

---
+++ Bibliotech/UserControls/CustomComboBox.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Bibliotech.UserControls
+{
+    /// <summary>
+    /// Interação lógica para ComboBox.xam
+    /// </summary>
+    public partial class CustomComboBox : UserControl
+    {
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(CustomComboBox), new PropertyMetadata("Title"));
+
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        public object SelectedItem
+        {
+            get => cb.SelectedItem;
+            set => cb.SelectedItem = true;
+        }
+
+        public int SelectedIndex
+        {
+            get => cb.SelectedIndex;
+            set => cb.SelectedIndex = value;
+        }
+
+        public IEnumerable ItemsSource
+        {
+            get => cb.ItemsSource;
+            set => cb.ItemsSource = value;
+        }
+
+        public CustomComboBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
