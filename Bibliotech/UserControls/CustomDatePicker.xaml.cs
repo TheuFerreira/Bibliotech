42de13207c5ad1d4dd32e1030d650ad8ad5dfc48
Matheus Ferreira <matheus.44300@gmail.com>
2021-08-11 11:41:52 -0300

    Update Style of Lending

---
+++ Bibliotech/UserControls/CustomDatePicker.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Bibliotech.UserControls
+{
+    /// <summary>
+    /// Interação lógica para CustomDatePicker.xam
+    /// </summary>
+    public partial class CustomDatePicker : UserControl
+    {
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(CustomDatePicker), new PropertyMetadata(string.Empty));
+
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        public DateTime? SelectedDate
+        {
+            get => date.SelectedDate;
+            set => date.SelectedDate = value;
+        }
+
+        public DateTime? DisplayDateStart
+        {
+            get => date.DisplayDateStart;
+            set => date.DisplayDateStart = value;
+        }
+
+        public DateTime? DisplayDateEnd
+        {
+            get => date.DisplayDateEnd;
+            set => date.DisplayDateEnd = value;
+        }
+
+        public CustomDatePicker()
+        {
+            InitializeComponent();
+        }
+    }
+}
