384b3b0d9d0993b8daf483e5c59d333d1c6a2ba5
Matheus Ferreira <matheus.44300@gmail.com>
2021-08-15 17:05:32 -0300

    Searching in Reports

--- Bibliotech/View/Reports/ReportsWindow.xaml.cs
+++ Bibliotech/View/Reports/ReportsWindow.xaml.cs
@@ -1,16 +1,13 @@
-﻿using System;
+﻿using Bibliotech.Model.DAO;
+using Bibliotech.Model.Entities;
+using Bibliotech.View.Reports.CustomEnums;
+using EnumsNET;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Bibliotech.View.Reports
 {
@@ -19,9 +16,114 @@ namespace Bibliotech.View.Reports
     /// </summary>
     public partial class ReportsWindow : Window
     {
+        private Tabs tabs;
+
+        private Period period;
+        private int month;
+        private int year;
+
+        private TypeLending typeLending;
+
+        private readonly DAOLending daoLending;
+
         public ReportsWindow()
         {
             InitializeComponent();
+
+            daoLending = new DAOLending();
+
+            tabs = Tabs.Lendings;
+
+            List<string> periodNames = Enum.GetValues(typeof(Period)).Cast<Period>().Select(x => x.AsString(EnumFormat.Description)).ToList();
+            lendingPeriod.ItemsSource = periodNames;
+
+            period = Period.Day;
+            lendingPeriod.SelectedItem = period.AsString(EnumFormat.Description);
+
+            lendingDate.SelectedDate = DateTime.Now;
+
+            List<string> monthName = Enumerable.Range(1, 12).Select(x => CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(x).ToUpper()).ToList();
+            lendingMonth.ItemsSource = monthName;
+
+            month = DateTime.Now.Month;
+            lendingMonth.SelectedItem = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(month).ToUpper();
+
+            List<int> years = new List<int>() { 2020, 2021 };
+            lendingYear.ItemsSource = years;
+
+            year = DateTime.Now.Year;
+            lendingYear.SelectedItem = year;
+
+            lendingStartDate.SelectedDate = DateTime.Now;
+            lendingEndDate.SelectedDate = DateTime.Now;
+
+            List<string> typeLendingNames = Enum.GetValues(typeof(TypeLending)).Cast<TypeLending>().Select(x => x.AsString(EnumFormat.Description)).ToList();
+            lendingType.ItemsSource = typeLendingNames;
+
+            typeLending = TypeLending.Both;
+            lendingType.SelectedItem = typeLending.AsString(EnumFormat.Description);
+        }
+
+        private void LendingPeriod_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selectedItem = lendingPeriod.SelectedItem.ToString();
+            period = Enums.Parse<Period>(selectedItem, true, EnumFormat.Description);
+
+            lendingDate.Visibility = Visibility.Collapsed;
+            lendingMonth.Visibility = Visibility.Collapsed;
+            lendingYear.Visibility = Visibility.Collapsed;
+
+            lendingStartDate.Visibility = Visibility.Collapsed;
+            lendingEndDate.Visibility = Visibility.Collapsed;
+
+            switch (period)
+            {
+                case Period.Day:
+                    lendingDate.Visibility = Visibility.Visible;
+                    break;
+                case Period.Mount:
+                    lendingMonth.Visibility = Visibility.Visible;
+                    break;
+                case Period.Year:
+                    lendingYear.Visibility = Visibility.Visible;
+                    break;
+                case Period.Custom:
+                    lendingStartDate.Visibility = Visibility.Visible;
+                    lendingEndDate.Visibility = Visibility.Visible;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void LendingMonth_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selectedItem = lendingMonth.SelectedItem.ToString();
+            DateTime dateMonth = DateTime.ParseExact(selectedItem, "MMMM", CultureInfo.CurrentCulture);
+            month = dateMonth.Month;
+        }
+
+        private void LendingYear_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selectedItem = lendingYear.SelectedItem.ToString();
+            year = int.Parse(selectedItem);
+        }
+
+        private void LendingType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selectedItem = lendingType.SelectedItem.ToString();
+            typeLending = Enums.Parse<TypeLending>(selectedItem, true, EnumFormat.Description);
+        }
+
+        private void BtnLendingExport_Click(object sender, RoutedEventArgs e)
+        {
+            throw new Exception("Falta Implementar a Exportação nos Empréstimos");
+        }
+
+        private async void BtnLendingSearch_Click(object sender, RoutedEventArgs e)
+        {
+            List<Lending> lendings = await daoLending.SearchLendings();
+            lendingDataGrid.ItemsSource = lendings;
         }
     }
 }
