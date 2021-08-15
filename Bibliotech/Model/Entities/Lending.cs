eb446ca7387040aac85617aecead314181736735
Matheus Ferreira <matheus.44300@gmail.com>
2021-08-15 16:32:37 -0300

    Update Lending.cs

--- Bibliotech/Model/Entities/Lending.cs
+++ Bibliotech/Model/Entities/Lending.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Bibliotech.Model.Entities
 {
-    class Lending
+    public class Lending
     {
+        public int IdLending { get; set; }
+        public Lector Lector { get; set; }
+        public User User { get; set; }
+        public DateTime LoanDate { get; set; }
+        public DateTime ExpectedDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+        public List<Exemplary> Exemplaries { get; set; }
     }
 }
