74c3beb2559d16be1f5174f5e2f7acfa9f01e4cc
Matheus Ferreira <matheus.44300@gmail.com>
2021-08-25 17:30:03 -0300

    Added new Converter

---
+++ Bibliotech/Converters/AuthorsValueConverter.cs
@@ -0,0 +1,35 @@
+﻿using Bibliotech.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Bibliotech.Converters
+{
+    public class AuthorsValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            List<Author> authors = value as List<Author>;
+
+            string text = string.Empty;
+            for (int i = 0; i < authors.Count; i++)
+            {
+                Author author = authors[i];
+                text += author.Name;
+
+                if (i < authors.Count - 1)
+                {
+                    text += ", ";
+                }
+            }
+
+            return text;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
