7c57613aa062a415a95d9f592274009f8f01cf8a
Matheus Ferreira <matheus.44300@gmail.com>
2021-09-16 17:25:24 -0300

    Update Connection.cs

--- Bibliotech/Model/DAO/Connection.cs
+++ Bibliotech/Model/DAO/Connection.cs
@@ -1,4 +1,6 @@
-﻿using MySqlConnector;
+﻿using Bibliotech.Services;
+using Bibliotech.Singletons;
+using MySqlConnector;
 using System.Data;
 using System.Threading.Tasks;
 
@@ -7,19 +9,20 @@ namespace Bibliotech.Model.DAO
     public abstract class Connection
     {
         protected MySqlConnection SqlConnection { get; private set; }
-        private const string CONNECTION_STRING = "Server=localhosts;Port=3306;Database=bibliotech;Username=bibliotech;Password=@bibliotech123;AllowZeroDateTime=true;Allow User Variables=true;";
+        //private const string CONNECTION_STRING = "Server=localhost;Port=3306;Database=bibliotech;Username=bibliotech;Password=@bibliotech123;AllowZeroDateTime=true;Allow User Variables=true;";
         //private const string CONNECTION_STRING = "Server=ns858.hostgator.com.br;Port=3306;Database=asifmg99_bibliotech;Username=asifmg99_bib;Password=@bibliotech2021;AllowZeroDateTime=true;Allow User Variables=true;";
 
         protected async Task Connect()
         {
             try
             {
-                //SqlConnection = new MySqlConnection(Session.Instance.Server.ToString());
-                SqlConnection = new MySqlConnection(CONNECTION_STRING);
+                SqlConnection = new MySqlConnection(Session.Instance.Server.ToString());
+                //SqlConnection = new MySqlConnection(CONNECTION_STRING);
                 await SqlConnection.OpenAsync();
             }
             catch (MySqlException ex)
             {
+                new DialogService().ShowError("Houve um problema ao se conectar com o servidor!!!");
                 throw ex;
             }
         }
